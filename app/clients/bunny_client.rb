166d98c904e2a490693befb0c79dbb1adf2d1691
armanmoradi <armanmoradi8@gmail.com>
2024-07-07 23:11:57 +0330

    create bunny clinet class

---
+++ app/clients/bunny_client.rb
@@ -0,0 +1,43 @@
+require "net/http"
+
+class BunnyClient
+  BASE_URI = "https://video.bunnycdn.com"
+
+  def initialize(library_id, access_key)
+    @library_id, @access_key = library_id, access_key
+  end
+
+  def get(path)
+    http_request Net::HTTP::Get, "/library/#{@library_id}#{path}"
+  end
+
+  def post
+    http_request Net::HTTP::Get, "/library/#{@library_id}#{path}", body: body
+  end
+
+  #http_request(Net::HTTP::Post, "/videos", body: {})
+  def http_request(method, path, body: nil)
+    uri = URI.parse("#{BASE_URI}#{path}")
+
+    http = Net::HTTP.new(uri.host, uri.post)
+    http.use_ssl = uri.is_a?(URI::HTTPS)
+
+    request = method.new(uri)
+    request["accept"] = "application/json"
+    request["AccessKey"] = @access_key
+
+    if request.is_a? Net::HTTP::Post
+      request.body = body.to_json
+      request["content-type"] = "application/json"
+    end
+
+    response = http.request(request)
+
+    if response.is_a? Net::HTTPSuccess
+      JSON.parse(response.body).with_indifferent_access
+    else
+      binding.irb
+      raise StandardError, "Request failed"
+    end
+  end
+end
